aa3c61f7e7518a0a5705032e50688b71992c5a7c
Sergey Efimov <yefimovsergey00@gmail.com>
2022-04-05 23:11:25 +0300

    Tests changed considering comments to homework

--- src/test/java/ru/netology/SelenideFormTest.java
+++ src/test/java/ru/netology/SelenideFormTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -15,8 +14,6 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideFormTest {
-    Calendar calendar = Calendar.getInstance();
-    SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy"); //устанавливаем формат даты
 
     String generateDate(int days, String formatPattern) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(formatPattern));
@@ -29,63 +26,53 @@ public class SelenideFormTest {
 
     //Далее идут тесты для выполнения задачи 1
     @Test
-    void shouldSendForm() {
-        $("[data-test-id='city'] input").val("Москва");
-        calendar.add(Calendar.DAY_OF_MONTH, 7); //добавляем 7 дней к текущей дате
-        $("[data-test-id='date'] input").doubleClick().sendKeys(dateFormat.format(calendar.getTime()));
-        $("[data-test-id='name'] input").val("Иванов Иван");
-        $("[data-test-id='phone'] input").val("+79991112233");
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Забронировать")).click();
-        $(withText("Успешно")).should(appear, Duration.ofSeconds(15));
-    }
-
-    //Использовал в тесте LocalDate
-    @Test
     void shouldSendFormUsingLocalDate() {
+        String planningDate = generateDate(7, "dd.MM.yyyy");
         $("[data-test-id='city'] input").val("Москва");
-        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(7, "dd.MM.yyyy"));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(planningDate);
         $("[data-test-id='name'] input").val("Иванов Иван");
         $("[data-test-id='phone'] input").val("+79991112233");
         $("[data-test-id='agreement'] span").click();
         $(withText("Забронировать")).click();
-        $(withText("Успешно")).should(appear, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
     }
 
-    //Использовал в тесте LocalDate
     @Test
     void shouldSendFormUsingLocalDateWithWrongDateFormat() {
+        int planningDateShift = 7;
+        String planningDate = generateDate(planningDateShift, "ddMM/yyyy");
         $("[data-test-id='city'] input").val("Москва");
-        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(7, "ddMM/yyyy"));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(planningDate);
         $("[data-test-id='name'] input").val("Иванов Иван");
         $("[data-test-id='phone'] input").val("+79991112233");
         $("[data-test-id='agreement'] span").click();
         $(withText("Забронировать")).click();
-        $(withText("Успешно")).should(appear, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на "
+                + generateDate(planningDateShift, "dd.MM.yyyy")), Duration.ofSeconds(15));
     }
 
     @Test
     void shouldAcceptЁ() {
+        String planningDate = generateDate(7, "dd.MM.yyyy");
         $("[data-test-id='city'] input").val("Москва");
-        calendar.add(Calendar.DAY_OF_MONTH, 7); //добавляем 7 дней к текущей дате
-        $("[data-test-id='date'] input").doubleClick().sendKeys(dateFormat.format(calendar.getTime()));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(planningDate);
         $("[data-test-id='name'] input").val("Семёнов Пётр");
         $("[data-test-id='phone'] input").val("+79991112233");
         $("[data-test-id='agreement'] span").click();
         $(withText("Забронировать")).click();
-        $(withText("Успешно")).should(appear, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
     }
 
     @Test
     void shouldAcceptDash() {
+        String planningDate = generateDate(7, "dd.MM.yyyy");
         $("[data-test-id='city'] input").val("Москва");
-        calendar.add(Calendar.DAY_OF_MONTH, 7); //добавляем 7 дней к текущей дате
-        $("[data-test-id='date'] input").doubleClick().sendKeys(dateFormat.format(calendar.getTime()));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(planningDate);
         $("[data-test-id='name'] input").val("Петрова-Водкина Анна-Мария");
         $("[data-test-id='phone'] input").val("+79991112233");
         $("[data-test-id='agreement'] span").click();
         $(withText("Забронировать")).click();
-        $(withText("Успешно")).should(appear, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
     }
 
     @Test
@@ -108,8 +95,7 @@ public class SelenideFormTest {
     @Test
     void shouldNotSendWithoutName() {
         $("[data-test-id='city'] input").val("Москва");
-        calendar.add(Calendar.DAY_OF_MONTH, 7); //добавляем 7 дней к текущей дате
-        $("[data-test-id='date'] input").doubleClick().sendKeys(dateFormat.format(calendar.getTime()));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(7, "dd.MM.yyyy"));
         $("[data-test-id='phone'] input").val("+79991112233");
         $("[data-test-id='agreement'] span").click();
         $(withText("Забронировать")).click();
@@ -119,8 +105,7 @@ public class SelenideFormTest {
     @Test
     void shouldNotSendWithoutPhone() {
         $("[data-test-id='city'] input").val("Москва");
-        calendar.add(Calendar.DAY_OF_MONTH, 7); //добавляем 7 дней к текущей дате
-        $("[data-test-id='date'] input").doubleClick().sendKeys(dateFormat.format(calendar.getTime()));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(7, "dd.MM.yyyy"));
         $("[data-test-id='name'] input").val("Иванов Иван");
         $("[data-test-id='agreement'] span").click();
         $(withText("Забронировать")).click();
@@ -130,8 +115,7 @@ public class SelenideFormTest {
     @Test
     void shouldNotSendWithoutAgreement() {
         $("[data-test-id='city'] input").val("Москва");
-        calendar.add(Calendar.DAY_OF_MONTH, 7); //добавляем 7 дней к текущей дате
-        $("[data-test-id='date'] input").doubleClick().sendKeys(dateFormat.format(calendar.getTime()));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(7, "dd.MM.yyyy"));
         $("[data-test-id='name'] input").val("Иванов Иван");
         $("[data-test-id='phone'] input").val("+79991112233");
         $(withText("Забронировать")).click();
@@ -141,8 +125,7 @@ public class SelenideFormTest {
     @Test
     void shouldNotSendWithoutSurname() {
         $("[data-test-id='city'] input").val("Москва");
-        calendar.add(Calendar.DAY_OF_MONTH, 7); //добавляем 7 дней к текущей дате
-        $("[data-test-id='date'] input").doubleClick().sendKeys(dateFormat.format(calendar.getTime()));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(7, "dd.MM.yyyy"));
         $("[data-test-id='name'] input").val("Иван");
         $("[data-test-id='phone'] input").val("+79991112233");
         $("[data-test-id='agreement'] span").click();
@@ -153,8 +136,7 @@ public class SelenideFormTest {
     @Test
     void shouldNotSendWithDashForName() {
         $("[data-test-id='city'] input").val("Москва");
-        calendar.add(Calendar.DAY_OF_MONTH, 7); //добавляем 7 дней к текущей дате
-        $("[data-test-id='date'] input").doubleClick().sendKeys(dateFormat.format(calendar.getTime()));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(7, "dd.MM.yyyy"));
         $("[data-test-id='name'] input").val("-");
         $("[data-test-id='phone'] input").val("+79991112233");
         $("[data-test-id='agreement'] span").click();
@@ -165,8 +147,7 @@ public class SelenideFormTest {
     @Test
     void shouldNotSendCityOutOfList() {
         $("[data-test-id='city'] input").val("Королев");
-        calendar.add(Calendar.DAY_OF_MONTH, 7); //добавляем 7 дней к текущей дате
-        $("[data-test-id='date'] input").doubleClick().sendKeys(dateFormat.format(calendar.getTime()));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(7, "dd.MM.yyyy"));
         $("[data-test-id='name'] input").val("Иванов Иван");
         $("[data-test-id='phone'] input").val("+79991112233");
         $("[data-test-id='agreement'] span").click();
@@ -178,8 +159,7 @@ public class SelenideFormTest {
     @Test
     void shouldNotAcceptDateInPast() {
         $("[data-test-id='city'] input").val("Москва");
-        calendar.add(Calendar.DAY_OF_MONTH, -7); //отнимаем 7 дней от текущей даты
-        $("[data-test-id='date'] input").doubleClick().sendKeys(dateFormat.format(calendar.getTime()));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(-7, "dd.MM.yyyy"));
         $("[data-test-id='name'] input").val("Иванов Иван");
         $("[data-test-id='phone'] input").val("+79991112233");
         $("[data-test-id='agreement'] span").click();
@@ -191,8 +171,7 @@ public class SelenideFormTest {
     @Test
     void shouldNotSendLatinName() {
         $("[data-test-id='city'] input").val("Москва");
-        calendar.add(Calendar.DAY_OF_MONTH, 7); //добавляем 7 дней к текущей дате
-        $("[data-test-id='date'] input").doubleClick().sendKeys(dateFormat.format(calendar.getTime()));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(7, "dd.MM.yyyy"));
         $("[data-test-id='name'] input").val("Ivanov Ivan");
         $("[data-test-id='phone'] input").val("+79991112233");
         $("[data-test-id='agreement'] span").click();
@@ -204,8 +183,7 @@ public class SelenideFormTest {
     @Test
     void shouldNotSendSpecialSymbolsForName() {
         $("[data-test-id='city'] input").val("Москва");
-        calendar.add(Calendar.DAY_OF_MONTH, 7); //добавляем 7 дней к текущей дате
-        $("[data-test-id='date'] input").doubleClick().sendKeys(dateFormat.format(calendar.getTime()));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(7, "dd.MM.yyyy"));
         $("[data-test-id='name'] input").val("!#&?");
         $("[data-test-id='phone'] input").val("+79991112233");
         $("[data-test-id='agreement'] span").click();
@@ -217,8 +195,7 @@ public class SelenideFormTest {
     @Test
     void shouldNotSendNumbersInName() {
         $("[data-test-id='city'] input").val("Москва");
-        calendar.add(Calendar.DAY_OF_MONTH, 7); //добавляем 7 дней к текущей дате
-        $("[data-test-id='date'] input").doubleClick().sendKeys(dateFormat.format(calendar.getTime()));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(7, "dd.MM.yyyy"));
         $("[data-test-id='name'] input").val("Иван 1");
         $("[data-test-id='phone'] input").val("+79991112233");
         $("[data-test-id='agreement'] span").click();
@@ -230,8 +207,7 @@ public class SelenideFormTest {
     @Test
     void shouldNotSendWithoutPlusInPhone() {
         $("[data-test-id='city'] input").val("Москва");
-        calendar.add(Calendar.DAY_OF_MONTH, 7); //добавляем 7 дней к текущей дате
-        $("[data-test-id='date'] input").doubleClick().sendKeys(dateFormat.format(calendar.getTime()));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(7, "dd.MM.yyyy"));
         $("[data-test-id='name'] input").val("Иванов Иван");
         $("[data-test-id='phone'] input").val("79991112233");
         $("[data-test-id='agreement'] span").click();
@@ -243,8 +219,7 @@ public class SelenideFormTest {
     @Test
     void shouldNotSendLessDigitsInPhone() {
         $("[data-test-id='city'] input").val("Москва");
-        calendar.add(Calendar.DAY_OF_MONTH, 7); //добавляем 7 дней к текущей дате
-        $("[data-test-id='date'] input").doubleClick().sendKeys(dateFormat.format(calendar.getTime()));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(7, "dd.MM.yyyy"));
         $("[data-test-id='name'] input").val("Иванов Иван");
         $("[data-test-id='phone'] input").val("+7999111222");
         $("[data-test-id='agreement'] span").click();
@@ -256,20 +231,22 @@ public class SelenideFormTest {
     //Далее идут тесты для выполнения задачи 2
     @Test
     void shouldUseCityList() {
-        $("[data-test-id='city'] input").val("ко");
-        $$(".menu-item__control").get(2).click();
-        $("[data-test-id='date'] input").doubleClick().sendKeys(generateDate(7, "dd.MM.yyyy"));
+        $("[data-test-id='city'] input").val("мо");
+        $$(".menu-item__control").findBy(text("Москва")).click();
+        String planningDate = generateDate(7, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(planningDate);
         $("[data-test-id='name'] input").val("Иванов Иван");
         $("[data-test-id='phone'] input").val("+79991112233");
         $("[data-test-id='agreement'] span").click();
         $(withText("Забронировать")).click();
-        $(withText("Успешно")).should(appear, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
     }
 
     @Test
     void shouldUseWebCalendarWithinCurrentMonth() {
         int defaultCalendarShift = 3;
         int planningDateShift = 7;
+        String planningDate = generateDate(planningDateShift, "dd.MM.yyyy");
         $("[data-test-id='city'] input").val("ко");
         $$(".menu-item__control").first().click();
         $("[data-test-id='date'] button").click();
@@ -282,25 +259,6 @@ public class SelenideFormTest {
         $("[data-test-id='phone'] input").val("+79991112233");
         $("[data-test-id='agreement'] span").click();
         $(withText("Забронировать")).click();
-        $(withText("Успешно")).should(appear, Duration.ofSeconds(15));
-    }
-
-    @Test
-    void shouldUseWebCalendarNextMonth() {
-        int defaultCalendarShift = 3;
-        int planningDateShift = 7;
-        $("[data-test-id='city'] input").val("ко");
-        $$(".menu-item__control").first().click();
-        $("[data-test-id='date'] button").click();
-        if (!generateDate(28 - defaultCalendarShift, "MM")
-                .equals(generateDate(28 - defaultCalendarShift + planningDateShift, "MM"))) {
-            $("[data-step='1'].calendar__arrow_direction_right").click();
-        }
-        $$("[data-day]").findBy(exactText(generateDate(planningDateShift, "d"))).click();
-        $("[data-test-id='name'] input").val("Иванов Иван");
-        $("[data-test-id='phone'] input").val("+79991112233");
-        $("[data-test-id='agreement'] span").click();
-        $(withText("Забронировать")).click();
-        $(withText("Успешно")).should(appear, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
     }
 }
